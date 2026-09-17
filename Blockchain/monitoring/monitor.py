0d6339a8ba63dd4c70b3a00c02392f34c81f6e89
asadtalcher-sudo <asadtalcher@gmail.com>
2026-09-18 02:31:19 +0530

    Process transfers for watched wallets

--- Blockchain/monitoring/monitor.py
+++ Blockchain/monitoring/monitor.py
@@ -13,3 +13,14 @@ class WalletMonitor:
 
     def get_watched_wallets(self):
         return list(self.watched_wallets)
+    
+    def process_transfer(self, from_address, to_address):
+        watched = []
+
+        if self.is_watched(from_address):
+            watched.append(from_address.lower())
+
+        if self.is_watched(to_address):
+            watched.append(to_address.lower())
+
+        return watched
