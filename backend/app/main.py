8cdb3de7b943d21ef0dc376a18b33f9272e02cf7
Pratyush Ranjan <pratyushranjan@Pratyushs-MacBook-Air.local>
2026-09-17 15:07:47 +0530

    Autorization

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.routers import cases, wallets
+from app.routers import cases, wallets, auth
 
 app = FastAPI(title="Project Acetone API", version="1.0.0")
 
 # Register the mock routers
+app.include_router(auth.router)
 app.include_router(cases.router)
 app.include_router(wallets.router)
 
